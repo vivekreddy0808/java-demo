ae617ef6e6791ace2f84fdf009b1e14f19179ad9
Vivek Reddy <vivekreddy@Viveks-MacBook-Pro.local>
2022-06-12 22:56:24 +0530

    Added demo Java file

--- demo.java
+++ demo.java
@@ -3,8 +3,3 @@ public class HelloWorld {
 	    System.out.println("Hello, World");
   }
 }
-
-public class VariblesDec {
-  public static void main(String[])
-      System.out.println("This is my first java program to run in jenkins build pipeline");
-}
