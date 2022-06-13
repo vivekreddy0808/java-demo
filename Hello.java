e3b79f08c305ed2028e426a7c6e7aa608f922073
Vivek Reddy <vivekreddy@Viveks-MacBook-Pro.local>
2022-06-13 14:44:46 +0530

    update

--- Hello.java
+++ Hello.java
@@ -20,6 +20,13 @@ public class HelloWorld {
   }
 }
 
+public class HelloWorld {
+  public static void main(String[] args) {
+	    System.out.println("Hello, World");
+      System.out.println("Hello, This is my first java program");
+  }
+}
+
 
 public class HelloWorld {
   public static void main(String[] args) {
